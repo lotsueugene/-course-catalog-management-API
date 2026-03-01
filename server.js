a1404b2d6efdd36682bb0d3b84fc71f31187d7ff
Eugene Lotsu <lotsue.24@gmail.com>
2026-02-28 22:55:58 -0600

    Implement GET endpoint for retrieving all courses

--- server.js
+++ server.js
@@ -8,3 +8,17 @@ app.use(express.json());
 
 // Connect to database
 const db = new sqlite3.Database('./database/university.db');
+
+
+// GET all courses
+app.get('/api/courses', (req, res) => {
+    db.all('SELECT * FROM courses', (err, rows) => {
+        res.json(rows);
+    });
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+})
