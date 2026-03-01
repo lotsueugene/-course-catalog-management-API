3db2c26796508e50d7a1e63fe2575452deb4afb6
Eugene Lotsu <lotsue.24@gmail.com>
2026-02-28 22:17:21 -0600

    table creation logic in setup.js

--- database/setup.js
+++ database/setup.js
@@ -2,7 +2,37 @@ const sqlite3 = require('sqlite3').verbose();
 
 
 // Create/connect to database file
-const db = new sqlite3.Database('./database/university.db');
-console.log('Connected to SQLite database');
+const db = new sqlite3.Database('./database/university.db', (error) =>{
+    if (error) {
+        console.log(error)
+    }
+    console.log('Connected to SQLite database');
+});
 
 
+
+// Create courses table
+db.run(`
+    CREATE TABLE IF NOT EXISTS courses (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        courseCode TEXT,
+        title TEXT,
+        credits INTEGER,
+        description TEXT,
+        semester TEXT
+    )
+`, (error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Courses table created');
+    }
+
+    db.close((err) => {
+        if (err) {
+            console.log(error);
+        } else {
+            console.log('Database connection closed');
+        }
+    });
+});
